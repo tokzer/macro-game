d5f65382dc926636faf1ce36463321c260148c2c
tokzer <johan.floren@gmail.com>
2025-07-20 14:19:55 +0200

    Added moving squares

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,74 @@
-use macroquad::prelude::*;
+use macroquad::{prelude::*};
+
+const MOVEMENT_SPEED: f32 = 200.0;
+const RADIUS: f32 = 16.0;
+
+struct Shape {
+    size: f32,
+    speed: f32,
+    x: f32,
+    y: f32,
+}
 
 #[macroquad::main("Macro Game")]
 async fn main() {
-        let mut x = screen_width() / 2.0;
-        let mut y = screen_height() / 2.0;
+    rand::srand(miniquad::date::now() as u64);
+
+    let mut squares: Vec<Shape> = vec![];
+    let mut circle = Shape {
+        size: RADIUS,
+        speed: MOVEMENT_SPEED,
+        x: screen_width() / 2.0,
+        y: screen_height() / 2.0,
+    };
 
     loop {
+
+        if rand::gen_range(0, 99) >= 95 {
+            let size = rand::gen_range(16.0, 64.0);
+            squares.push(Shape {
+                size: size,
+                speed: rand::gen_range(50.0, 150.0),
+                x: rand::gen_range(size / 2.0, screen_width() - size / 2.0), 
+                y: -size,
+            });
+        }
         clear_background(DARKPURPLE);
+
+        let delta_time = get_frame_time();
         
         if is_key_down(KeyCode::Right) {
-            x += 1.0;
+            circle.x += circle.speed * delta_time;
         }
         if is_key_down(KeyCode::Left) {
-            x -= 1.0;
+            circle.x -= circle.speed * delta_time;
         }
         if is_key_down(KeyCode::Down) {
-            y += 1.0;
+            circle.y += circle.speed * delta_time;
         }
         if is_key_down(KeyCode::Up) {
-            y -= 1.0;
+            circle.y -= circle.speed * delta_time;
         }
 
-        draw_circle(x, y, 16.0, YELLOW);
-        
+        circle.x = clamp(circle.x, 0.0 + RADIUS / 2.0, screen_width() - RADIUS / 2.0);
+        circle.y = clamp(circle.y, 0.0 + RADIUS / 2.0, screen_height() - RADIUS / 2.0);
+        draw_circle(circle.x, circle.y, circle.size, YELLOW);
+
+        for square in &mut squares {
+            square.y += square.speed * delta_time;
+        }
+
+        squares.retain(|square| square.y < screen_height() + square.size);
+
+        for square in &squares {
+            draw_rectangle(
+                square.x - square.size / 2.0,
+                square.y - square.size / 2.0,
+                square.size,
+                square.size, GREEN,
+            );
+        }
+
         next_frame().await;
     }
 }
