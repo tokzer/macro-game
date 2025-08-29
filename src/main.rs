2d6e7f36f989245ae2c824e3da0ba4084635d73c
tokzer <johan.floren@gmail.com>
2025-08-29 13:57:11 +0200

    Added menu

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use macroquad::audio::{load_sound, play_sound, play_sound_once, PlaySoundParams};
 use macroquad::experimental::animation::{AnimatedSprite, Animation};
+use macroquad::ui::{hash, root_ui, Skin};
 use macroquad::prelude::*;
 use macroquad_particles::{self as particles, AtlasConfig, Emitter, EmitterConfig};
 
@@ -172,6 +173,50 @@ async fn main() {
         PlaySoundParams { looped: true, volume: 1., },
     );
 
+    let window_background = load_image("window_background.png").await.unwrap();
+    let button_background = load_image("button_background.png").await.unwrap();
+    let button_clicked_background = load_image("button_clicked_background.png").await.unwrap();
+    let font = load_file("atari_games.ttf").await.unwrap();
+
+    let window_style = root_ui()
+        .style_builder()
+        .background(window_background)
+        .background_margin(RectOffset::new(32.0, 76.0, 44.0, 20.0))
+        .margin(RectOffset::new(0.0, -40.0, 0.0, 0.0))
+        .build();
+
+    let button_style = root_ui()
+        .style_builder()
+        .background(button_background)
+        .background_clicked(button_clicked_background)
+        .background_margin(RectOffset::new(16.0, 16.0, 16.0, 16.0))
+        .margin(RectOffset::new(16.0, 0.0, -8.0, -8.0))
+        .font(&font)
+        .unwrap()
+        .text_color(WHITE)
+        .font_size(64)
+        .build();
+
+    let label_style = root_ui()
+        .style_builder()
+        .font(&font)
+        .unwrap()
+        .text_color(WHITE)
+        .font_size(28)
+        .build();
+
+    let ui_skin = Skin {
+        window_style,
+        button_style,
+        label_style,
+        ..root_ui().default_skin()
+    };
+
+    root_ui().push_skin(&ui_skin);
+    let window_size = vec2(370.0, 320.0);
+
+    
+
     // let mut direction_modifier: f32 = 0.0;
     // let render_target = render_target(320, 150);
     // render_target.texture.set_filter(FilterMode::Nearest);
@@ -211,10 +256,16 @@ async fn main() {
 
         match game_state {
             GameState::MainMenu => {
-                if is_key_pressed(KeyCode::Escape){
-                    std::process::exit(0);
-                }
-                if is_key_pressed(KeyCode::Space){
+                root_ui().window(
+            hash!(), 
+            vec2(
+                screen_width() / 2.0 - window_size.x / 2.0,
+                screen_height() / 2.0 - window_size.y / 2.0,
+            ),
+            window_size,
+            |ui| {
+                ui.label(vec2(80.0, -34.0), "Main menu");
+                if ui.button(vec2(65.0, 25.0), "Play") {
                     squares.clear();
                     bullets.clear();
                     explosions.clear();
@@ -223,14 +274,10 @@ async fn main() {
                     score = 0;
                     game_state = GameState::Playing;
                 }
-                let text = "Press space";
-                let text_dimensions = measure_text(text, None, 50, 1.0);
-                draw_text(
-                    text,
-                    screen_width() / 2.0 - text_dimensions.width / 2.0,
-                    screen_height() / 2.0,
-                    50.0,
-                    WHITE);
+                if ui.button(vec2(65.0, 125.0), "Quit") {
+                    std::process::exit(0);
+                }
+            });
             },
             GameState::Playing => {
                 let delta_time = get_frame_time();
